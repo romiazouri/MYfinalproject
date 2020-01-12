60e37ff88b3830f9b21af13b8319dbde3224e31b
גד לידרור <gad.li1@tlvedu.org.il>
2020-01-12 19:49:38 +0200

    Add project files.

---
+++ DemoFormProject/views.py
@@ -0,0 +1,103 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from DemoFormProject import app
+
+
+from datetime import datetime
+from flask import render_template, redirect, request
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import json 
+import requests
+
+import io
+import base64
+
+from os import path
+
+from flask   import Flask, render_template, flash, request
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import TextField, TextAreaField, SubmitField, SelectField, DateField
+from wtforms import ValidationError
+
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+
+@app.route('/Album')
+def Album():
+    """Renders the about page."""
+    return render_template(
+        'PictureAlbum.html',
+        title='Pictures',
+        year=datetime.now().year,
+        message='Welcome to my picture album'
+    )
+
+
+@app.route('/Query', methods=['GET', 'POST'])
+def Query():
+    print ("Running from query()")
+    Name = None
+    capital = ''
+    form = CountryForm()
+    df = pd.read_csv(path.join(path.dirname(__file__), 'static\\Data\\capitals.csv'))
+    df = df.set_index('Country')
+    if (form.validate_on_submit()):
+        name = form.name.data
+        if (name in df.index):
+            capital = df.loc[name,'Capital']
+        else:
+            capital = name + ', no such country'
+        form.name.data = ''
+    #raw_data_table = df.to_html(classes = 'table table-hover')
+    return render_template('Query.html', 
+        form = form, 
+        name = capital, 
+        raw_data_table = df.to_html(classes = 'table table-hover'),
+        title='Query by the user',
+        year=datetime.now().year,
+        message='This page will use the web forms to get user input'
+    )
+
+
